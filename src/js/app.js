4ba57e4fd6fbf0e7da4a57d45b24c314b63cf884
AlexseiT <alex.todorov@mail.ru>
2024-02-27 17:37:17 +0700

    add calculator

--- src/js/app.js
+++ src/js/app.js
@@ -208,31 +208,121 @@ function CheckCheckBox(){
 		}
 	});
 }
-function LayoutDetailDropDown(){
-	let form = document.querySelector('.layout-detail__form');
-	if (form)
-	{
-		let select = document.querySelector('.layout-detail__select');
-		let selectSize = select.options.length;
-		let svg = document.querySelector('.layout-detail__svg');
-		form.addEventListener('click', (event) => {
-			//button.classList.toggle('button__noactive');
-			//select.click();
-			//select.focus();
-			//select.size = selectSize ;
-			//form.classList.toggle('layout-detail__form_active')
-			//svg.classList.toggle('layout-detail__svg_active')
-		});
-		select.addEventListener('blur', () => {
-			select.size = 1;
-		  })
+
+function AddRangeSliders() {
+	let slider_price = document.querySelector('.hypothec__input-price');
+	let slider_contribution = document.querySelector('.hypothec__input-contribution');
+	let slider_term = document.querySelector('.hypothec__input-term');
+  
+	let ui_slider_price = noUiSlider.create(slider_price, {
+	  start: 4480000,
+	  connect: true,
+	  range: {
+		'min': 4480000,
+		'max': 8194000
+	  },
+	  step: 1
+	});
+	let ui_slider_contribution = noUiSlider.create(slider_contribution, {
+	  start: 15,
+	  connect: true,
+	  range: {
+		'min': 15,
+		'max': 100
+	  },
+	  step: 1
+	});
+	let ui_slider_term = noUiSlider.create(slider_term, {
+	  start: 10,
+	  connect: true,
+	  range: {
+		'min': 1,
+		'max': 30
+	  },
+	  step: 1
+	});
+  
+	let text_price = document.querySelector(".hypothec__input-text-price");
+	let text_contribution = document.querySelector(".hypothec__input-text-contribution");
+	let text_term = document.querySelector(".hypothec__input-text-term");
+	let text_percent = document.querySelector(".hypothec__input-text-percent");
+	
+	let price;
+	text_price.addEventListener("change", function() {
+	  ui_slider_price.set(parseFloat(this.value));
+	});
+  
+	text_contribution.addEventListener("change", function() {
+	  ui_slider_contribution.set(parseFloat(this.value));
+	});
+  
+	text_term.addEventListener("change", function() {
+	  ui_slider_term.set(parseInt(this.value));
+	});
+
+	ui_slider_price.on('update', function(values, handle) {
+		text_price.value = Math.round(values[handle]);
+		price = Math.round(values[handle]);
+	});
+	
+	ui_slider_contribution.on('update', function(values, handle) {
+
+		let contributionPercentage = values[handle]; 
+		let contributionPrice = (price * contributionPercentage) / 100; // Вычисляем цену, которая является процентом первоначального взноса от цены
+		text_contribution.value = contributionPrice;
+		text_percent.value = Math.round(values[handle]) + "%";
+	});
+
+	ui_slider_term.on('update', function(values, handle) {
+	text_term.value = Math.round(values[handle]);
+	});
+
+
+		// Функция для пересчета ежемесячного платежа и его обновления
+	function updateMonthlyPayment(bank) {
+		const percent = parseFloat(bank.querySelector('.hypothec__bank-percent').textContent);
+		const monthlyRate = percent / 12 / 100;
+		
+		const loanAmount = ui_slider_price.get() - ui_slider_contribution.get();
+		const term = ui_slider_term.get() * 12;
+		const rate =  (1 + monthlyRate) ** term;
+
+		const monthlyPayment = (loanAmount * monthlyRate * rate) / (rate - 1)//(1 - Math.pow(1 + monthlyRate, term));
+		const formattedPayment = monthlyPayment.toFixed(0);
+
+		bank.querySelector('.hypothec__bank-price').textContent = Math.round(formattedPayment) + " руб. /мес";
 	}
-}
+
+	// Получаем все блоки с классом hypothec__bank
+	const banks = document.querySelectorAll('.hypothec__bank');
+
+	// Обработчик события для ползунка цены
+	ui_slider_price.on('update', () => {
+		banks.forEach(bank => {
+			updateMonthlyPayment(bank);
+		});
+	});
+
+	// Обработчик события для ползунка первоначального взноса
+	ui_slider_contribution.on('update', () => {
+		banks.forEach(bank => {
+			updateMonthlyPayment(bank);
+		});
+	});
+
+	// Обработчик события для поля ввода срока кредита
+	ui_slider_term.on('update', () => {
+		banks.forEach(bank => {
+			updateMonthlyPayment(bank);
+		});
+	});
+  }
+
 document.addEventListener('DOMContentLoaded', (event) => {
     CallbackFormInit();
     InitCenteredSliders();
 	CheckCheckBox();
-	//LayoutDetailDropDown();
+	AddRangeSliders();
     // particlesJS.load('particles-slider', 'static/ParticlesJSON/GreenHexagons.json');
 
     // Содержание статьи по заголовкам
